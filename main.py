95de320eac75abd9caad840edb9683c018d82435
devilediting994-art <devilediting994@gmail.com>
2026-08-23 09:31:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ LANG_FILE = "languages.json"
 
 START_PHOTO = "https://kommodo.ai/i/IOLcEhfHnTNODGFnUBQI"
 HELP_PHOTO = START_PHOTO
-FORCE_CHANNEL = "@JP_NETWORK"
-SUPPORT_LINK = "https://t.me/jp_network"
+FORCE_CHANNEL = "@TH3GALAXY"
+SUPPORT_LINK = "https://t.me/+SNQtX4745Pc1MTY1"
 
 JOIN_TIME = 30
 MAX_LEVELS = 10
